16c11e3ed05e44122a2adff6312148f0366aa16d
Kyu hyunSung <dmp100@naver.com>
2025-03-20 14:10:47 +0900

    Initial Commit

---
+++ IosSwiftUI/IosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosSwiftUIApp.swift
+//  IosSwiftUI
+//
+//  Created by 성규현 on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct IosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
